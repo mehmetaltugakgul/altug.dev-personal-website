e125c88ec6d94ad60fd2d3dbd2541a2d4b39a970
Mehmet Altuğ Akgül <mehmetaltugakgul@papilon.com.tr>
2022-01-22 00:04:51 +0300

    Updates

---
+++ precache-manifest.bdd979da1a347f604eace122e660fac9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2afdd7eaf56327664055753d202db3",
+    "url": "/altug.dev-personal-website/index.html"
+  },
+  {
+    "revision": "2e46d5dd352ea0822d28",
+    "url": "/altug.dev-personal-website/static/css/main.6055dea8.chunk.css"
+  },
+  {
+    "revision": "19ea3c14620953974d79",
+    "url": "/altug.dev-personal-website/static/js/2.8363ed28.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/altug.dev-personal-website/static/js/2.8363ed28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e46d5dd352ea0822d28",
+    "url": "/altug.dev-personal-website/static/js/main.e5dc9cd0.chunk.js"
+  },
+  {
+    "revision": "5e45a22c883155103193",
+    "url": "/altug.dev-personal-website/static/js/runtime-main.e4fcb07b.js"
+  },
+  {
+    "revision": "b7ac566c8affeba86dc3ba0c88794cea",
+    "url": "/altug.dev-personal-website/static/media/1.b7ac566c.jpg"
+  },
+  {
+    "revision": "fed4eeae5ed1252e77ebc2186d36c608",
+    "url": "/altug.dev-personal-website/static/media/Resim1.fed4eeae.png"
+  },
+  {
+    "revision": "69335a211dcb6b99db586362c8ee78d4",
+    "url": "/altug.dev-personal-website/static/media/fokusAkademiCalisma-e1512823236190.69335a21.png"
+  },
+  {
+    "revision": "2b74e8381583853ffc9ed0d7c6ce4ab8",
+    "url": "/altug.dev-personal-website/static/media/logo pg.2b74e838.png"
+  },
+  {
+    "revision": "6522a9da55e5e7f0f07311ac4766533b",
+    "url": "/altug.dev-personal-website/static/media/vatozlogo.6522a9da.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cecca247082510cccf8c1683529667da.js"
+  "/altug.dev-personal-website/precache-manifest.bdd979da1a347f604eace122e660fac9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/altug.dev-personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
