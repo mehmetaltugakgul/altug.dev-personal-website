d5706ebe1fe262a21d8fb306814c9b9ceca0269f
Mehmet Altuğ Akgül <mehmetaltugakgul@papilon.com.tr>
2022-01-21 21:15:20 +0300

    Updates

---
+++ precache-manifest.ff9a76f5cc74b8f3d882a20fd98c6597.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6205b42d3b8917089083eb08abb20d93",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ffe0d8ce234974b3092c",
+    "url": "/home/static/css/main.6055dea8.chunk.css"
+  },
+  {
+    "revision": "eb2c4c1211b929aed5ea",
+    "url": "/home/static/js/2.c205c789.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.c205c789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe0d8ce234974b3092c",
+    "url": "/home/static/js/main.f22cb7c3.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "b7ac566c8affeba86dc3ba0c88794cea",
+    "url": "/home/static/media/1.b7ac566c.jpg"
+  },
+  {
+    "revision": "fed4eeae5ed1252e77ebc2186d36c608",
+    "url": "/home/static/media/Resim1.fed4eeae.png"
+  },
+  {
+    "revision": "69335a211dcb6b99db586362c8ee78d4",
+    "url": "/home/static/media/fokusAkademiCalisma-e1512823236190.69335a21.png"
+  },
+  {
+    "revision": "2b74e8381583853ffc9ed0d7c6ce4ab8",
+    "url": "/home/static/media/logo pg.2b74e838.png"
+  },
+  {
+    "revision": "6522a9da55e5e7f0f07311ac4766533b",
+    "url": "/home/static/media/vatozlogo.6522a9da.png"
+  }
+]);
